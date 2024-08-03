9d34629144b452742deaea71e527814a306e0811
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-08-03 08:56:13 +0530

    Deleted ML-CodeSpace

--- Python_Code_Space_For_ML/5-Modules/test.py
+++
@@ -1,6 +0,0 @@
-from package.math import *
-from package.subpackage.maths import mul
-
-print(add(2, 3))
-print(sub(4, 2))
-print(mul(4, 5))
--- Python_Code_Space_For_ML/test.py
+++
@@ -1,7 +0,0 @@
-print(1 + 1)
-
-## This is single line comment in Python
-
-'''
-This is multi line comment in Python
-'''
